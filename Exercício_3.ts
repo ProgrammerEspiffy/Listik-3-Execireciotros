a408ff3ad8ef49200b3613c986f963b2e80e5ab9
Natan Henrique De Marcos Rego E Baptista <natanbaptista@brq.com>
2022-10-14 17:07:58 -0300

    i Commited little mistake sorry :(

--- Exercício_3.ts
+++ Exercício_3.ts
@@ -29,7 +29,7 @@ class Felinos extends Animais {
 }
 // Chamando os Métodos do "Felinos"
 let Felino1 = new Felinos("Pequeno", 3, 5)
-Felino1.AfiarGarras(); Felino1.Dormir
+Felino1.AfiarGarras(); Felino1.Dormir();
 
 let Felino2 = new Felinos("Grande", 10, 50)
-Felino2.AfiarGarras(); Felino2.Dormir
+Felino2.AfiarGarras(); Felino2.Dormir();
